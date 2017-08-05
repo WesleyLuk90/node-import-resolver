d621249a52fa602693a66ea5c2ab8909aaa27cb9
Wesley Luk <wesleyluk@gmail.com>
2017-08-05 14:10:59 -0700

    Match token by selection

--- spec/TokenFinder-spec.js
+++ spec/TokenFinder-spec.js
@@ -1,7 +1,7 @@
 'use babel';
 
 import TokenFinder from '../lib/TokenFinder';
-import { Point } from 'atom';
+import { Point, Range } from 'atom';
 
 describe('TokenFinder', () => {
     let editor;
@@ -13,8 +13,8 @@ describe('TokenFinder', () => {
     });
 
     it('should find no tokens', () => {
-        const tokenFinder = new TokenFinder(editor);
-        expect(tokenFinder.getTokens()).toEqual(null);
+        const finder = new TokenFinder(editor);
+        expect(finder.getToken()).toEqual(null);
     });
 
     describe('with cursor position', () => {
@@ -26,20 +26,58 @@ more data`);
 
         it('should find a highlighted token when cursor is in the middle', () => {
             editor.setCursorBufferPosition(new Point(1, 3));
-            const tokenFinder = new TokenFinder(editor);
-            expect(tokenFinder.getTokens()).toEqual('findAToken');
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toEqual('findAToken');
         });
 
         it('should find a highlighted token when cursor is at the start', () => {
             editor.setCursorBufferPosition(new Point(1, 0));
-            const tokenFinder = new TokenFinder(editor);
-            expect(tokenFinder.getTokens()).toEqual('findAToken');
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toEqual('findAToken');
         });
 
         it('should find a highlighted token when cursor is at the end', () => {
             editor.setCursorBufferPosition(new Point(1, 10));
-            const tokenFinder = new TokenFinder(editor);
-            expect(tokenFinder.getTokens()).toEqual('findAToken');
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toEqual('findAToken');
         });
-    })
+    });
+
+    describe('with selection', () => {
+        beforeEach(() => {
+            editor.setText(`some random info
+@#$% findAToken !@#$
+more data`);
+        });
+
+        it('should not find an invalid token', () => {
+            editor.setSelectedBufferRange(new Range(new Point(1, 1), new Point(1, 3)));
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toBe(null);
+        });
+
+        it('should find an exactly selected token', () => {
+            editor.setSelectedBufferRange(new Range(new Point(1, 6), new Point(1, 15)));
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toBe("findAToken");
+        });
+
+        it('should find a partially selected token', () => {
+            editor.setSelectedBufferRange(new Range(new Point(1, 8), new Point(1, 10)));
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toBe("findAToken");
+        });
+
+        it('should find a exactly token at the start of a selection', () => {
+            editor.setSelectedBufferRange(new Range(new Point(1, 6), new Point(1, 20)));
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toBe("findAToken");
+        });
+
+        it('should fail to find a token not at the start', () => {
+            editor.setSelectedBufferRange(new Range(new Point(1, 0), new Point(1, 15)));
+            const finder = new TokenFinder(editor);
+            expect(finder.getToken()).toBe(null);
+        });
+    });
 });
